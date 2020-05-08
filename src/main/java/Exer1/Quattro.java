70e94377d4e4940d2f64bd7d0d1602d147615ae2
alromanenko <alromanenko@playtika.com>
2020-05-08 18:45:18 +0300

    proba string

---
+++ src/main/java/Exer1/Quattro.java
@@ -0,0 +1,12 @@
+package Exer1;
+
+public class Quattro {
+
+    public static void main(String[] args) {
+        String star="*";
+        String space=" ";
+        String courceName="Course from Home: Java core";
+        String stud="Student";
+        String fio="Alexandro Romanenko";
+    }
+}
